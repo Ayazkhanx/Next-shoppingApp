e2cb64b02ba5c17bc1b983788a1e44d2eba52fa7
Ayazkhanx <143736083+Ayazkhanx@users.noreply.github.com>
2024-12-12 15:04:36 +0500

    initial commit

--- hackathon/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function loading() {
-  return (
-    <div>
-      <h1 className='text-6xl text-center min-h-svh min-w-full'>Loading ! Please Wait</h1>
-    </div>
-  )
-}
